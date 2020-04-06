a1e716147e2ca7787ce6156ddbefab1ff49fef76
smenaaliaga <smenaaliaga@gmail.com>
2020-04-06 12:43:38 -0400

    Se generaliza el codigo del template

--- sitioweb/views.py
+++ sitioweb/views.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.shortcuts import render, HttpResponse, Http404
 from django.urls import path
+from django.shortcuts import render, HttpResponse, Http404
 
 def home(request) :
     return render(request, "home.html")
+
+def about(request) :
+    return render(request, "about.html")
--- thesimoel/urls.py
+++ thesimoel/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from sitioweb.views import home
+from sitioweb.views import home, about
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', home),
+    path('about/', about),
     path('skynet/', admin.site.urls),
 ]
